eac29d6f0fb13d544e9953b8a51c742088019fbf
Montezi <mirianmontezi.pro@gmail.com>
2019-07-24 23:46:41 -0300

    chore: Criação e Layout Home e Cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-asics-gel-nimbus-20-boston-feminino/58/D18-2423-058/D18-2423-058_zoom1.jpg"
+          alt="tênis"
+        />
+        <strong>Tênis Top</strong>
+        <span>R$129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
